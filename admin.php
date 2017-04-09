ee2485de57c35ebce1d5706ff06a40cba3649231
Samuel Valdez <savaldez@csumb.edu>
2017-04-09 06:56:08 +0000

    added log in process for admin.

---
+++ admin.php
@@ -0,0 +1,1 @@
+<h1>you got in bruh</h1>
---
+++ loginProcess.php
@@ -0,0 +1,40 @@
+<?php
+session_start();  //start or resume an existing session
+
+
+
+include '../inc/dbConnection.php';
+
+$conn = getDBConnection("blockbuster");
+
+$username = $_POST['username'];
+$password = md5($_POST['password']);   //hash("sha1",$_POST['password']);
+
+//USE NAMEDPARAMETERS TO PREVENT SQL INJECTION
+//$sql = "SELECT * FROM admin WHERE username = '$username' AND password = '$password'";
+$sql = "SELECT * FROM admin WHERE username = :username AND password = :password";
+
+$namedParameters[':username'] = $username;
+$namedParameters[':password'] = $password;
+
+
+$statement = $conn->prepare($sql);
+$statement->execute($namedParameters);
+$record = $statement->fetch(PDO::FETCH_ASSOC);
+
+//print_r($record);
+
+ if (empty($record)) { //wrong credentials
+     
+     echo "Wrong username or password";
+     
+ } else {
+     
+     $_SESSION["adminName"] = $record['firstName'] . " " . $record['lastName'];
+     $_SESSION['username'] = $record['username'];
+     header("Location: admin.php"); //redirect to the main admin program
+     
+ }
+
+
+?>
