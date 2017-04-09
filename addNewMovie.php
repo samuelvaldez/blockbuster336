e26ef0bd764603e95e02061ec03f4d244e1cdab6
Samuel Valdez <savaldez@csumb.edu>
2017-04-09 08:20:32 +0000

    added the add movie page, can now add into db 'movies' using page

---
+++ addNewMovie.php
@@ -0,0 +1,113 @@
+<?php
+
+
+session_start();
+
+if (!isset($_SESSION["username"])) {  //Check whether the admin has logged in
+    header("Location: login.html"); 
+}
+
+
+
+include '../inc/dbConnection.php';
+
+$conn = getDBConnection("blockbuster");
+
+include 'includes/phpfunctions.php';
+
+function addMovie(){
+    global $conn;
+       $sql = "INSERT INTO movies(
+       id,
+       title,
+       year,
+       rating,
+       runtime,
+       genre,
+       director, 
+       review, 
+       rentalCost,
+       checkoutStatus
+       )
+       VALUES (
+           :id,:title,:year,:rating,:runtime,:genre,:director,:review,:rentalCost,:checkoutStatus
+           )";
+    $movieArray = array() ;
+    $movieArray[':id']= $_GET['id'];
+    $movieArray[':title']= $_GET['title'];
+    $movieArray[':year']= $_GET['year'];
+    $movieArray[':rating']= $_GET['rating'];
+    $movieArray[':runtime']= $_GET['runtime'];
+    $movieArray[':genre']= $_GET['genre'];
+    $movieArray[':director']= $_GET['director'];
+    $movieArray[':review']= $_GET['review'];
+    $movieArray[':rentalCost']= $_GET['rentalCost'];
+    $movieArray[':checkoutStatus']= $_GET['checkoutStatus'];
+    
+    $stmt = $conn -> prepare ($sql);
+    $stmt -> execute($movieArray);
+}
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Admin: Add New MOVIE </title>
+    </head>
+    <body>
+        
+        <h1> Add New Movie</h1>
+        
+        <form>
+            
+            Movie Id: <input type = "text" name = "id"/>
+        
+            <br/>
+
+            Title: <input type = "text" name = "title"/>
+            <br/>
+            Year: <input type = "text" name = "year"/>
+                        <br/>
+            Rating:<select name = "rating">
+                <option value = "G">G</option>
+                <option value = "PG">PG</option>
+                <option value = "PG-13">PG-13</option>
+                <option value = "R">R</option>
+                <option value = "NC-17">NC-17</option>
+            </select>
+            <br/>
+            Runtime: <input type = "text" name = "runtime"/>
+                        <br/>
+            Genre: <input type = "text" name = "genre"/>
+                        <br/>
+            Director: <input type = "text" name = "director"/>
+                        <br/>
+            Review: <input type = "text" name = "review"/>
+                        <br/>           
+            Rental Cost: <input type = "text" name = "rentalCost"/>
+                        <br/>
+            CheckOut Status: 
+              <input type="radio" name="checkoutStatus" value="1"> Available<br>
+              <input type="radio" name="checkoutStatus" value="0"> Unavailable<br>
+              
+            <br/>
+            
+             <input type="submit" name="Submit"> 
+        </form>
+    <?php
+        if (isset($_GET['Submit'])){
+            //echo "form was submitted";
+            addMovie();
+            echo "THe user was added sucessfully!";
+        }
+    ?>
+
+
+
+    </body>
+</html>
--- admin.php
+++ admin.php
@@ -1,1 +1,66 @@
-<h1>you got in bruh</h1>
+<?php
+session_start();
+
+if (!isset($_SESSION["username"])) {  //Check whether the admin has logged in
+    header("Location: login.html"); 
+}
+
+include '../inc/dbConnection.php';
+
+$dbConn = getDBConnection("blockbuster");
+
+
+function getAllMovies() {
+    global $dbConn;
+    $sql = "SELECT * FROM movies ORDER BY title";
+    $statement = $dbConn->prepare($sql);
+    $statement->execute();
+    $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    return $records;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> ADMIN PAGE </title>
+    </head>
+    <body>
+
+        <h1> NOT ADMIN </h1>
+        
+        <h3> Welcome <?=$_SESSION['adminName']?> </h3>
+        
+        
+        
+        
+        <hr>
+        
+        <form action="addNewMovie.php">
+          <input type="submit" value="Add New Movie" />
+        </form>
+        
+        
+        
+        
+        <div style="float:left">
+        <?php
+        
+        $movies = getAllMovies();
+        
+        
+        foreach($movies as $movie){
+            echo $movie['title'] . " ".  $movie['checkoutStatus'];
+            echo "<a href='userUpdate.php?userId=". $user['userid']. "' >[Update] </a>";
+        }
+        
+        ?>
+       
+        </div>
+        
+        <div style="float:right">
+        <iframe src ="" width="400" height="400"name = "userInfoFrame"></iframe>
+        </div>
+    </body>
+</html>
